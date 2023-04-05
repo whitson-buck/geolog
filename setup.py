f2227b03b17671b697815fc4dec473e9f5233d51
whitson-buck <whitsonbuck@gmail.com>
2023-04-05 16:14:38 -0400

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/whitson-buck/geolog',
-    version='0.0.7',
+    version='0.0.8',
     zip_safe=False,
 )
