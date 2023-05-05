d5e27e25816b36ad50018f2d5db5fe8b33cb7a2e
whitson-buck <whitsonbuck@gmail.com>
2023-05-04 22:39:47 -0400

    to_streamlit

--- geolog/geolog.py
+++ geolog/geolog.py
@@ -309,6 +309,57 @@ class Map(ipyleaflet.Map):
 
         self.add_control(toolbar_ctrl)
 
+    def to_streamlit(
+        self,
+        width=None,
+        height=600,
+        scrolling=False,
+        add_layer_control=True,
+        bidirectional=False,
+        **kwargs,
+    ):
+        """Renders `folium.Figure` or `folium.Map` in a Streamlit app. This method is a static Streamlit Component, meaning, no information is passed back from Leaflet on browser interaction.
+
+        Args:
+            width (int, optional): Width of the map. Defaults to None.
+            height (int, optional): Height of the map. Defaults to 600.
+            scrolling (bool, optional): Whether to allow the map to scroll. Defaults to False.
+            add_layer_control (bool, optional): Whether to add the layer control. Defaults to True.
+            bidirectional (bool, optional): Whether to add bidirectional functionality to the map. The streamlit-folium package is required to use the bidirectional functionality. Defaults to False.
+
+        Raises:
+            ImportError: If streamlit is not installed.
+
+        Returns:
+            streamlit.components: components.html object.
+        """
+
+        try:
+            import streamlit.components.v1 as components
+
+            if add_layer_control:
+                self.add_layer_control()
+
+            if bidirectional:
+                from streamlit_folium import st_folium
+
+                output = st_folium(self, width=width, height=height)
+                return output
+            else:
+                # if responsive:
+                #     make_map_responsive = """
+                #     <style>
+                #     [title~="st.iframe"] { width: 100%}
+                #     </style>
+                #     """
+                #     st.markdown(make_map_responsive, unsafe_allow_html=True)
+                return components.html(
+                    self.to_html(), width=width, height=height, scrolling=scrolling
+                )
+
+        except Exception as e:
+            raise Exception(e)
+
     # #     # widget_width = "250px"
     # #     # padding = "0px 0px 0px 5px"
 
