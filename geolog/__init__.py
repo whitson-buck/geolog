8660d525c894d1e9a78581ad09b0add1679e577b
whitson-buck <whitsonbuck@gmail.com>
2023-11-28 14:36:28 -0500

    0.1.0!

--- geolog/__init__.py
+++ geolog/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Whitson Buck"""
 __email__ = 'whitsonbuck@gmail.com'
-__version__ = '0.0.9'
+__version__ = '0.1.0'
 
 from .geolog import *
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/whitson-buck/geolog',
-    version='0.0.9',
+    version='0.1.0',
     zip_safe=False,
 )
